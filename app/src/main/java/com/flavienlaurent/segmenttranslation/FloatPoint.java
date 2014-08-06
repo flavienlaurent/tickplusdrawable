2dfca6237991400ba7bdbffda3e508f3a027832c
Flavien Laurent <flavien.laurent@gmail.com>
2014-08-06 17:30:40 +0200

    first push

---
+++ app/src/main/java/com/flavienlaurent/segmenttranslation/FloatPoint.java
@@ -0,0 +1,19 @@
+package com.flavienlaurent.segmenttranslation;
+
+public class FloatPoint {
+    float x;
+    float y;
+
+    public FloatPoint(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "FloatPoint{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
